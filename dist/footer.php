3b44e00b53c8b18dd8e397986b59b0958f84385a
Matt Hummel <marketing@users.noreply.github.com>
2020-06-24 10:57:35 -0400

    Update
    add bebas nue, and robot fonts

--- dist/footer.php
+++ dist/footer.php
@@ -11,11 +11,15 @@
 
 ?>
 
-
   <!-- Footer -->
   <footer class="footer bg-light">
     <div class="container">
       <div class="row">
+        <div class="col-lg-12">
+        <a href="https://www.germanna.edu"><img src="https://78bbm3rv7ks4b6i8j3cuklc1-wpengine.netdna-ssl.com/wp-content/uploads/gcc-logo.svg" alt="white germanna logo" height="108" width="147" class="logo"/></a>
+        </div>
+    </div>
+   <div class="row">
         <div class="col-lg-6 h-100 text-center text-lg-left my-auto">
           <ul class="list-inline mb-2">
             <li class="list-inline-item">
@@ -30,7 +34,7 @@
               <a href="#">Accreditation</a>
             </li>
           </ul>
-          <p class="text-muted small mb-4 mb-lg-0">&copy; Your Website 2020. All Rights Reserved.</p>
+          <p class="text-muted small mb-4 mb-lg-0">&copy; Germanna Community College.</p>
         </div>
         <div class="col-lg-6 h-100 text-center text-lg-right my-auto">
           <ul class="list-inline mb-0">
