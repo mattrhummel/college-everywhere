8b97ec272eff4e08a946b16d6de89206790172a8
Matt Hummel <marketing@users.noreply.github.com>
2020-06-30 15:48:31 -0400

    Update
    center footer

--- footer.php
+++ footer.php
@@ -10,17 +10,16 @@
  */
 
 ?>
-  <!-- Footer -->
   <footer class="footer bg-light">
     <div class="container">
       <div class="row">
-        <div class="col-lg-6 h-100 text-center text-lg-left my-auto">
+        <div class="col-lg-6 h-100 text-center my-auto">
         <a href="https://www.germanna.edu"><img src="https://78bbm3rv7ks4b6i8j3cuklc1-wpengine.netdna-ssl.com/wp-content/uploads/gcc-logo.svg" alt="white germanna logo" height="108" width="147" class="logo"/></a>
         <p class="mx-sm-auto">Powered by Germanna Community College</p>
         </div>
     </div>
    <div class="row">
-        <div class="col-lg-6 h-100 text-center text-lg-left my-auto">
+        <div class="col-lg-6 h-100 text-center my-auto">
           <ul class="list-inline mb-2">
             <li class="list-inline-item">
               <a href="https://www.germanna.edu/about-germanna/privacy-policy/">Privacy</a>
