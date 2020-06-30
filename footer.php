4aa82092ede5b816f8d785910b932cda1dfa8fdd
Matt Hummel <marketing@users.noreply.github.com>
2020-06-30 15:55:00 -0400

    Update
    full width footer

--- footer.php
+++ footer.php
@@ -13,13 +13,13 @@
   <footer class="footer bg-light">
     <div class="container">
       <div class="row">
-        <div class="col-lg-6 h-100 text-center my-auto">
+        <div class="col-lg-12 h-100 text-center my-auto">
         <a href="https://www.germanna.edu"><img src="https://78bbm3rv7ks4b6i8j3cuklc1-wpengine.netdna-ssl.com/wp-content/uploads/gcc-logo.svg" alt="white germanna logo" height="108" width="147" class="logo"/></a>
         <p class="mx-sm-auto">Powered by Germanna Community College</p>
         </div>
     </div>
    <div class="row">
-        <div class="col-lg-6 h-100 text-center my-auto">
+        <div class="col-lg-12 h-100 text-center my-auto">
           <ul class="list-inline mb-2">
             <li class="list-inline-item">
               <a href="https://www.germanna.edu/about-germanna/privacy-policy/">Privacy</a>
@@ -34,8 +34,6 @@
             </li>
           </ul>
           <p class="text-muted small mb-4 mb-lg-0">&copy; Germanna Community College.</p>
-        </div>
-        <div class="col-lg-6 h-100 text-center text-lg-right my-auto">
         </div>
       </div>
     </div>
