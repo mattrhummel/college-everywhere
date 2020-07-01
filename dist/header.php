3a609e159065990c1262df684a0aca1f402366b1
Matt Hummel <marketing@users.noreply.github.com>
2020-07-01 16:14:05 -0400

    Update
    callout row

--- dist/header.php
+++ dist/header.php
@@ -8,16 +8,13 @@
  *
  * @package college_everywhere
  */
-
 ?>
 <!doctype html>
 <html <?php language_attributes(); ?>>
 <head>
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"><link rel="profile" href="https://gmpg.org/xfn/11">
-
 	<?php wp_head(); ?>
-
 </head>
 
 <body <?php body_class(); ?>>
