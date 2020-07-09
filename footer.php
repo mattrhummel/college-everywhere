d1e0ef39a5da9c6bad2950a9925bb97b8f4f961d
Matt Hummel <marketing@users.noreply.github.com>
2020-07-09 13:28:58 -0400

    update
    more padding in footer

--- footer.php
+++ footer.php
@@ -10,7 +10,7 @@
  */
 
 ?>
-  <footer class="footer bg-light" style="padding-bottom: 170px;">
+  <footer class="footer bg-light" style="padding-bottom: 300px;">
     <div class="container">
       <div class="row">
         <div class="col-lg-12 h-100 text-center my-auto">
