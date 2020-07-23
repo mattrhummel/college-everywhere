045a6992d83968b1fa992f8de96b266e40a0f063
Matt Hummel <marketing@users.noreply.github.com>
2020-07-23 11:32:26 -0400

    update
    revised get started

--- footer.php
+++ footer.php
@@ -41,22 +41,15 @@
 <!-- Flexbox container for aligning the toasts -->
 <div class="toast text-center" data-autohide="false" style="position: fixed; z-index: 10000; right: 0; bottom: 20px; background-color: #fbf065; width: 300px;">
   <div class="toast-header" style="background-color: #fbf065;">
-    <h5 class="text-dark">Get Started</h5>
+    <h5 class="text-dark text-center">Get Started</h5>
    
     <button type="button" class="ml-auto mb-1 close" data-dismiss="toast" aria-label="Close">
     <span aria-hidden="true">&times;</span>
     </button>
 
   </div>
-  <div class="toast-body" style="background-color: #fbf065">
-    <div class="d-none d-md-block">
-   <img src="https://fivegstaging.wpengine.com/wp-content/uploads/GCCstaff006-1.jpg" alt="christian tomkins" class="img-fluid img-thumbnail mb-4">
-   <h6>Pleased to—virtually—meet you</h6>
-   <p>Our online, accelerated degree programs are a smart option for getting to your future fast. I’m here to answer any of your questions and help you get started.</p>
-
-  <p>Christian Tompkins<br/>
-  <em>Enrollment Services</em></p>
-</div>
+  <div class="toast-body text-center" style="background-color: #fbf065">
+    <p>Register by August 7. Classes start August 10</p>
    <button class="btn btn-dark btn-block" type="button" data-toggle="collapse" data-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample">
     Request Information
   </button>
@@ -64,8 +57,16 @@
     Book an online session
   </a>
 </p>
-<div class="collapse" id="collapseExample">
-  <div class="card card-body" style="background-color: #fbf065; border: none;">
+<div class="collapse p-0" id="collapseExample">
+  <div class="card card-body p-0 pb-2" style="background-color: #fbf065; border: none;">
+      <div class="d-none d-md-block">
+   <img src="https://fivegstaging.wpengine.com/wp-content/uploads/GCCstaff006-1.jpg" alt="christian tomkins" class="img-fluid img-thumbnail mb-4">
+   <h6>Pleased to—virtually—meet you</h6>
+   <p>Our online, accelerated degree programs are a smart option for getting to your future fast. I’m here to answer any of your questions and help you get started.</p>
+
+  <p>Christian Tompkins<br/>
+  <em>Enrollment Services</em></p>
+</div>
     <?php echo do_shortcode( '[wpforms id="17"]'); ?>
   </div>
 </div>
