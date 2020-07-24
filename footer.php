0dcb847596183a7eaed0faecce26a43d3d6b6a23
Matt Hummel <marketing@users.noreply.github.com>
2020-07-24 14:36:21 -0400

    update
    toast reveal

--- footer.php
+++ footer.php
@@ -38,56 +38,25 @@
       </div>
     </div>
   </footer>
-<!-- Flexbox container for aligning the toasts -->
-<div class="toast text-center" data-autohide="false" style="position: fixed; z-index: 10000; right: 0; bottom: 20px; background-color: #fbf065; width: 350px;">
-  <div class="toast-header text-center" style="background-color: #fbf065;">
-   
-    <button type="button" class="ml-auto mb-1 close" data-dismiss="toast" aria-label="Close">
-    <span aria-hidden="true">&times;</span>
-    </button>
 
-  </div>
-  <div class="toast-body text-center" style="background-color: #fbf065">
-    <h5 class="text-dark text-center">Get Started</h5>
-    <p class="text-dark">Register by August 7. Classes start August 10</p>
-   <button class="btn btn-dark btn-block" type="button" data-toggle="collapse" data-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample">
-    Request Information
-  </button>
-     <a href="https://calendly.com/ctompkins-2/germanna-community-college-college-everywhere" class="btn btn-dark btn-block mb-2">
-    Book an online session
-  </a>
-</p>
-<div class="collapse p-0" id="collapseExample">
-  <div class="card card-body p-0 pb-2" style="background-color: #fbf065; border: none;">
-      <div class="d-none d-md-block">
-        <div class="container">
-      <div class="row">
-        <div class="col-lg-6">
-   <img src="https://fivegstaging.wpengine.com/wp-content/uploads/GCCstaff006-1.jpg" alt="christian tomkins" class="img-fluid img-thumbnail mb-4">
- </div>
- <div class="col-lg-6">
-   <h6 class="text-dark text-left">Pleased to—virtually—meet you</h6>
-   <p class="text-dark text-left">Our online, accelerated degree programs are a smart option for getting to your future fast. I’m here to answer any of your questions and help you get started.</p>
-
-  <p class="text-dark">Christian Tompkins<br/>
-  <em class="text-dark">Enrollment Services</em></p>
-  </div>
-</div>
-<div class="container">
-  <div class="row">
-      <div class="col">
-  <?php echo do_shortcode( '[wpforms id="17"]'); ?>
-</div>
-</div>
-</div>
-  </div>
-</div>
 
 <?php wp_footer(); ?>
 <script>
 $(document).ready(function(){
   $('.toast').toast('show');
 });
+</script>
+<script>
+$(document).scroll(function() {
+    if ($(this).scrollTop()>100)
+    {
+        $('.toast').fadeIn();
+    }
+    else
+     {
+      $('.toast').fadeOut();
+     }
+ });
 </script>
 </body>
 </html>
